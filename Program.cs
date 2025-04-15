d739eee005877aa12c2ed1b41bc3539d742d0445
l0mini <l0mini@naver.com>
2025-04-15 17:34:00 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿namespace TextRPG
+{
+    internal class Program
+    {
+        class GamePlace() 
+        {
+            void StartSence()
+            {
+                Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+                Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+                Console.WriteLine();
+                Console.WriteLine("1. 상태보기");
+                Console.WriteLine("2. 인벤토리");
+                Console.WriteLine("3. 상점");
+                Console.WriteLine();
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                int choice = int.Parse(Console.ReadLine());
+            }
+            //시작화면
+            //상태 보기
+            //인벤토리
+            //장착관리
+            //상점
+        }
+        class Player()
+        {
+            int level = 1;
+            string name = "플레이어";
+            string jop = "전사";
+            int attackPower = 10;
+            int defensePower = 5;
+            int health = 100;
+            int haveGold = 1500;
+        }
+
+        class Inventory()
+        {
+
+        }
+
+        class ItemEquipped() 
+        { 
+
+        }
+
+        class Shop()
+        {
+
+        }
+
+        class Shopping()
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상점");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.ReadLine();
+        }
+    }
+}
